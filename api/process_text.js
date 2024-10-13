8c1695e13341fdfb868b094b718a38e498dcc633
Brent Wise <brentjosephwise@gmail.com>
2024-10-13 15:28:26 -0700

    SPEED READ - Version 1.2.2 Alpha - Debugging

--- api/process_text.js
+++ api/process_text.js
@@ -1,11 +1,14 @@
 export default (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*'); // Allow all origins
+
   if (req.method === 'POST') {
     const { text } = req.body;
-    const words = text.split(/\s+/); // Split text into words
-    res.status(200).json(words);     // Return the array of words
+    const words = text.split(/\s+/);
+    res.status(200).json(words);
   } else {
     res.status(405).json({ message: 'Only POST requests allowed' });
   }
 };
+
 
   
